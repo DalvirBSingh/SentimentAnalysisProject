ee3d93b8cbd7d55d4b15a919c1486303d13e8b18
Dalvir Singh <dalvir.singh@cerner.com>
2020-12-06 12:15:07 -0500

    Both models working

---
+++ backend-service/server/apps/ml/classifier/LogisticRegression2.py
@@ -0,0 +1,42 @@
+
+import joblib
+import pandas as pd
+
+class LogisticRegressionClassifier2:
+    def __init__(self):
+        path_to_artifacts = "../machine-learning/"
+        #Dataset 1 predictive models
+        self.lg_model =  joblib.load("/Users/ds070111/Documents/GitHub/SentimentAnalysisProject/backend-service/server/machine-learning/lg_model2.joblib")
+        self.vectorizer = joblib.load("/Users/ds070111/Documents/GitHub/SentimentAnalysisProject/backend-service/server/machine-learning/vectorizer2.joblib")
+        self.tfidf = joblib.load("/Users/ds070111/Documents/GitHub/SentimentAnalysisProject/backend-service/server/machine-learning/tfidf2.joblib")
+        
+        #Dataset 2 predictive models
+
+    def processing(self, input_data):
+        # JSON to pandas DataFrame
+        
+        print(input_data)
+        data = [input_data]
+
+        user_data_df = pd.DataFrame(data, columns = ['text']) 
+        
+        user_data_features = self.vectorizer.transform(user_data_df['text'])
+
+        user_data_tfidf_features = self.tfidf.fit_transform(user_data_features)
+
+        return self.lg_model.predict(user_data_tfidf_features)
+        
+        
+    def compute_prediction(self, input_data):
+        try:
+            print(input_data)
+            prediction = self.processing(input_data)
+
+            if(prediction[0] == 1):
+                return {"label": "Happy", "status": "OK"}
+            elif(prediction[0] == 2):
+                return {"label": "Neutral", "status": "OK"}
+            else:
+                return {"label": "Sad", "status": "OK"}
+        except Exception as e:
+            return {"status": "Error", "message": str(e)}
--- backend-service/server/server/wsgi.py
+++ backend-service/server/server/wsgi.py
@@ -8,11 +8,13 @@ application = get_wsgi_application()
 import inspect
 from apps.ml.classifier.registry import MLRegistry
 from apps.ml.classifier.LogisticRegression import LogisticRegressionClassifier
+from apps.ml.classifier.LogisticRegression2 import LogisticRegressionClassifier2
 
 try:
     registry = MLRegistry() # create ML registry
     # Random Forest classifier
     lg = LogisticRegressionClassifier()
+    lg2 = LogisticRegressionClassifier2()
     # add to ML registry
     registry.add_algorithm(endpoint_name="consumer-reviews-of-amazon-products_classifier",
                             algorithm_object=lg,
@@ -22,6 +24,14 @@ try:
                             owner="Dalvir Singh",
                             algorithm_description="Logistic Regression Classifier trained on Consumer Reviews of Amazon Products",
                             algorithm_code=inspect.getsource(LogisticRegressionClassifier))
+    registry.add_algorithm(endpoint_name="food_review_classifier",
+                            algorithm_object=lg2,
+                            algorithm_name="Logistic Regression Classifier",
+                            algorithm_status="production",
+                            algorithm_version="0.0.1",
+                            owner="Dalvir Singh",
+                            algorithm_description="Logistic Regression Classifier trained on Food Reviews on Amazon",
+                            algorithm_code=inspect.getsource(LogisticRegressionClassifier2))
 
 except Exception as e:
     print("Exception while loading the algorithms to the registry,", str(e))
