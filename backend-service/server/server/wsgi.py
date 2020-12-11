52dd82cafb0879b2cb3d3a6a0c1f50769eaf3dea
Dalvir Singh <dalvir.singh@cerner.com>
2020-12-11 16:19:34 -0500

    delete stuff

--- backend-service/server/server/wsgi.py
+++ backend-service/server/server/wsgi.py
@@ -7,23 +7,14 @@ application = get_wsgi_application()
 # ML registry
 import inspect
 from apps.ml.classifier.registry import MLRegistry
-from apps.ml.classifier.LogisticRegression import LogisticRegressionClassifier
 from apps.ml.classifier.LogisticRegression2 import LogisticRegressionClassifier2
 
 try:
     registry = MLRegistry() # create ML registry
     # Random Forest classifier
-    lg = LogisticRegressionClassifier()
     lg2 = LogisticRegressionClassifier2()
     # add to ML registry
-    registry.add_algorithm(endpoint_name="consumer-reviews-of-amazon-products_classifier",
-                            algorithm_object=lg,
-                            algorithm_name="Logistic Regression Classifier",
-                            algorithm_status="production",
-                            algorithm_version="0.23.2",
-                            owner="Dalvir Singh",
-                            algorithm_description="Logistic Regression Classifier trained on Consumer Reviews of Amazon Products",
-                            algorithm_code=inspect.getsource(LogisticRegressionClassifier))
+
     registry.add_algorithm(endpoint_name="food_review_classifier",
                             algorithm_object=lg2,
                             algorithm_name="Logistic Regression Classifier",
